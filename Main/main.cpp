7a98ce902538fb05ddada3dd45b1189308ecce85
Cats Godot <135250387+CatsGodot@users.noreply.github.com>
2023-06-01 17:36:44 -0700

    Implemented menu functionality

--- Main/main.cpp
+++ Main/main.cpp
@@ -4,11 +4,27 @@
 //
 //  Created by Grant Guenther on 6/1/23.
 //
+//=======================================================================================
+//---------------------------------------------------------------------------------------
 
-#include <iostream>
+#include "TestMenus.hpp"
+
+//---------------------------------------------------------------------------------------
 
 int main(int argc, const char * argv[]) 
 {
+	MenuDeckGnome				menu_deck;
+	
+	TestMenu_Main				*main_menu = new TestMenu_Main();
+	TestMenu_Inner				*inner_menu = new TestMenu_Inner();
+	
+	menu_deck.addMenuToDeck(main_menu);
+	menu_deck.addMenuToDeck(inner_menu);
+	
+	while ( -1 != menu_deck.run() ) ;
 	
 	return 0;
 }
+
+//---------------------------------------------------------------------------------------
+//=======================================================================================
